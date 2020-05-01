6dc4e23a7a134736c18d3340b3c5fe824cdbcd3a
Javier Hevia <javierhc22@gmail.com>
2020-05-01 14:11:06 -0600

    update pagos

--- server.js
+++ server.js
@@ -267,5 +267,172 @@ app.put('/Afiliado', (req, res2) => {
   }
 })
 
+app.get('/Pago', (req, res2) => {
+  var theUrl = url.parse(req.url, true)
+  var autorizacion = false
+  jwt.verify(theUrl.query.jwt, public_key, opts, function (err, decoded) {
+    if (err) {
+      var respuesta = JSON.parse('{ "cod":403, "err":"El JWT no es valido o no contiene el scope de este servicio"}')
+      res2.send(respuesta)
+    } else {
+      const scope = JSON.parse(decoded.scope)
+      // EJEMPLO DE COMO LEER EL SCOPE
+      let access = ''
+      access = scope.find(element => element.toLowerCase() == 'vehiculo.get')
+
+      if (access != undefined) {
+        // SI TIENE ACCESO
+        autorizacion = true
+      }
+    }
+  })
+
+  if (autorizacion) {
+    console.log(theUrl.query)
+    var json2 = null
+    if (theUrl.query.codigo == "") {
+      json2 = null
+    } else {
+      // var queryObj = queryString.parse(theUrl.query)
+      // console.log(queryObj)
+      var qdata = theUrl.query
+      if (qdata.codigo !== undefined) {
+        var iddd = parseInt(qdata.codigo)
+        var parajson = '{' + '\"codigo\":' + iddd + '}'
+        json2 = JSON.parse(parajson)
+        console.log(json2)
+      }
+    }
+
+    var MongoClient = require('mongodb').MongoClient
+    var uri = 'mongodb://admin1:admin@cluster0-shard-00-00-k6sn1.mongodb.net:27017,cluster0-shard-00-01-k6sn1.mongodb.net:27017,cluster0-shard-00-02-k6sn1.mongodb.net:27017/Base1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+    function Insert (json) {
+      MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
+        if (err) throw err
+        var dbo = client.db('Base1') // .collection('Base1')
+        if (json == null) {
+          dbo.collection('Usuario').find({}).toArray(function (err, res) {
+            if (err) throw err
+            console.log('Dato Encontrado Correctamente')
+            // console.log(res)
+            client.close()
+            var resull = { response: res }
+            res2.send(res)
+          })
+        } else {
+          dbo.collection('Usuario').find(json).toArray(function (err, res) {
+            if (err) throw err
+            console.log('Dato Encontrado Correctamente find')
+            // console.log(res)
+            client.close()
+            if (res.length === 0) {
+              var respuesta = JSON.parse('{ "cod":404, "state":"Not found"}')
+              res2.send(respuesta)
+            } else {
+              var resull = { response: res }
+              res2.send(res)
+              // res2.send(res)
+            }
+          })
+        }
+      })
+      return true
+    }
+    Insert(json2)
+  }
+})
+
+app.post('/Pago', (req, res2) => {
+  var autorizacion = false
+  // console.log(req.body)
+  // console.log(req.body.name)
+  var theUrl = url.parse(req.url, true)
+  jwt.verify(theUrl.query.jwt, public_key, opts, function (err, decoded) {
+    if (err) {
+      var respuesta = JSON.parse('{ "cod":403, "err":"El JWT no es valido o no contiene el scope de este servicio"}')
+      res2.send(respuesta)
+    } else {
+      const scope = JSON.parse(decoded.scope)
+      // EJEMPLO DE COMO LEER EL SCOPE
+      let access = ''
+      access = scope.find(element => element.toLowerCase() == 'vehiculo.get')
+
+      if (access != undefined) {
+        // SI TIENE ACCESO
+        autorizacion = true
+      }
+    }
+  })
+  if (autorizacion) {
+    console.log(theUrl.query)
+
+    var anews = parseInt(theUrl.query.codigo)
+    var datoid = { _id: anews }
+    console.log(datoid)
+    var newdato = ''
+    if (theUrl.query.monto == '') {
+      var respuesta = JSON.parse('{ "cod":406, "state":"Not Acceptable"}')
+      return res2.send(respuesta)
+    } else {
+      var hoy = new Date()
+      var dd = hoy.getDate()
+      var mm = hoy.getMonth() + 1
+      var yyyy = hoy.getFullYear()
+      var hh = hoy.getHours()
+      var mmi = hoy.getMinutes()
+      var ss = hoy.getSeconds()
+
+      var fechas = yyyy + '-' + mm + '-' + dd + ' ' + hh + ':' + mmi + ':' + ss 
+
+      var estain = parseInt(theUrl.query.codigo)
+      var mmonto = parseFloat(theUrl.query.monto)
+      newdato = { $set: { _id: estain, monto: mmonto, fecha: fechas } }
+    } 
+    // newdato = { $set: { estado: theUrl.query.estado, afiliado: theUrl.query.afiliado_adjudicado, valor_adjudicado: theUrl.query.valor_adjudicado } }
+    console.log(newdato)
+
+    var MongoClient = require('mongodb').MongoClient
+    var uri = 'mongodb://admin1:admin@cluster0-shard-00-00-k6sn1.mongodb.net:27017,cluster0-shard-00-01-k6sn1.mongodb.net:27017,cluster0-shard-00-02-k6sn1.mongodb.net:27017/Base1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+    function update (obj1, newdato) {
+      MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
+        if (err) throw err
+        var dbo = client.db('Base1') // .collection('Base1')
+        dbo.collection('Usuario').find(obj1).toArray(function (err, res) {
+          if (err) throw err
+          // console.log('Dato Encontrado Correctamente')
+          console.log(res)
+          // client.close()
+          if (res.length === 0) {
+            var respuesta = JSON.parse('{ "cod":404, "state":"Not found"}')
+            res2.send(respuesta)
+            // console.log('vacio')
+          } else {
+            dbo.collection('Usuario').updateOne(obj1, newdato, function (err, res) {
+              if (err) throw err
+              console.log('Dato Encontrado Correctamente')
+              // console.log(res)
+              client.close()
+              // var respuesta = JSON.parse('{ "cod":201, "state":"Created"}')
+              var sdfe = {
+                  cod: datoid._id,
+                  nombre: theUrl.query.nombre,
+                  vigente: false
+                }
+                res2.send(sdfe)
+              //var resull = { response: true }
+              //res2.send(resull)
+            })
+            console.log('Conexion Exitosa')
+          }
+        })
+      })
+      return true
+    }
+    update(datoid, newdato)
+  }
+})
+
 app.listen(PORT, HOST)
 console.log('Servidor levantado http://' + HOST + ':' + PORT)
